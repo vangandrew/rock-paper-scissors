5e489280c1a5484eeceb1c9702b359eac27fe0af
Andrew Vang <andrw.vang@gmail.com>
2025-01-27 18:06:07 -0600

    Add logic and functionality to scripts.js Created getComputerChoice function and getHumanChoice function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,31 @@
-console.log("Hello Andrew!!!!!! You are so awesome and I believe in you... You are the best!")
+function getComputerChoice() {
+    /* Rock = Math.random(0 - 0.33), Paper = Math.random(0.34 - 0.66), Scissors = Math.random(0.67 - 1)  */
+    let result = Math.random();
+    if (result < 0.34) {
+        console.log("Rock");
+        console.log(result);
+    }
+    else if (result < 0.67) {
+        console.log("Paper");
+        console.log(result);
+    }
+    else if (result > 0.67) {
+        console.log("Scissors");
+        console.log(result);
+    }
+};
+
+getComputerChoice();
+
+function getHumanChoice(input) {
+    choices = ["Rock", "Paper", "Scissors"];
+    let result = prompt("Please enter your choice!");
+    if (input in choices) {
+        console.log(result);
+    }
+    else {
+        input = prompt("Not a valid choice... Please enter a valid choice!");
+    }
+}
+
+getHumanChoice();
