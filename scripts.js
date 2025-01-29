3f88403995e233eb4956b8528d69fc1b4fbcf8cf
Andrew Vang <andrw.vang@gmail.com>
2025-01-28 23:07:02 -0600

    Update getHumanChoice function and add playRound function

--- scripts.js
+++ scripts.js
@@ -1,31 +1,55 @@
 function getComputerChoice() {
     /* Rock = Math.random(0 - 0.33), Paper = Math.random(0.34 - 0.66), Scissors = Math.random(0.67 - 1)  */
     let result = Math.random();
+    let computerChoice = null;
+
     if (result < 0.34) {
-        console.log("Rock");
-        console.log(result);
+        computerChoice = "Rock";
     }
     else if (result < 0.67) {
-        console.log("Paper");
-        console.log(result);
+        computerChoice = "Paper";
     }
     else if (result > 0.67) {
-        console.log("Scissors");
-        console.log(result);
+        computerChoice = "Scissors";
     }
+
+    return computerChoice
 };
 
-getComputerChoice();
 
-function getHumanChoice(input) {
-    choices = ["Rock", "Paper", "Scissors"];
-    let result = prompt("Please enter your choice!");
-    if (input in choices) {
-        console.log(result);
+function getHumanChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    let result = prompt("Please enter your move: ");
+    while (!choices.includes(result)) {
+        result = prompt("Not a valid move... Please enter a valid move: ").trim().toLowerCase()
+        }
+    return result
+    }
+
+
+const humanChoice = console.log(`You chose: ${getHumanChoice()}`);
+const computerChoice = console.log(`The computer chose: ${getComputerChoice()}`);
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(computerChoice, playerChoice) {
+    if (playerChoice === computerChoice) {
+        console.log("Tie!!");
+    }
+    else if (
+        (playerChoice == "rock" && computerChoice == "scissors") ||
+        (playerChoice == "paper" && computerChoice == "rock") ||
+        (playerChoice == "scissors" && computerChoice == "paper")
+    ) {
+        console.log("Human won!");
+        humanScore++;
     }
     else {
-        input = prompt("Not a valid choice... Please enter a valid choice!");
+        console.log("Computer won!");
+        computerScore++;
     }
 }
 
-getHumanChoice();
+playRound(computerChoice, humanChoice);
+console.log(`Human: ${humanScore} vs Computer: ${computerScore}`);
